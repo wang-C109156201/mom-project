4c633c32f5d269dadb18d7f0dea342f38dd7803c
C109156201 <C109156201@nkust.edu.tw>
2022-08-06 17:34:05 +0800

    momproject

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$(".box").on({
+    "dragstart": function(event){
+      console.log("dragstart");
+    event.originalEvent.dataTransfer.setData('text/plain',event.target.id)
+    },
+    "dragend": function(){
+      console.log("dragend");
+      $(".target").removeClass("over")
+    }
+  })
+  
+  $(".target").on({
+    "dragenter": function(event){
+      event.preventDefault();
+      
+      console.log("dragenter");
+    },
+    "dragover": function(event){
+      event.preventDefault();
+      $(this).addClass("over")
+      
+      console.log("dragover");
+      
+    },
+    "dragleave": function(){
+      console.log("dragleave");
+      $(".target").removeClass("over")
+      
+    },
+    "drop": function(event){
+      event.preventDefault();
+      // event.stopPropagation(); //停止事件氣泡現象
+      console.log("drop");
+      let id = event.originalEvent.dataTransfer.getData('text/plain');
+      $("#"+id).appendTo(event.target);
+    }
+  })
