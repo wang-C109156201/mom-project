51539df9daea0ba53b291cf728718128c8d214d7
ho-chi <60824gi@gmail.com>
2024-05-29 20:09:41 +0800

    change

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ loginButton.disabled = true;
 
 password.onkeyup = function () {
     // 如果密码为 "class1024"，且其他验证条件也通过
-    if (password.value === "20240529" && username.value === "20240529") {
+    if (password.value === "2024end" && username.value === "2024end") {
         // console.log("pass1206");
         // 启用登录按钮
         loginButton.disabled = false;
