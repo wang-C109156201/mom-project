c2ad08345a573e99e82bee4884e58797d0b6f8f0
wang-C109156201 <82928036+wang-C109156201@users.noreply.github.com>
2025-04-29 20:41:09 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ loginButton.disabled = true;
 
 password.onkeyup = function () {
     // 如果密码为 "class1024"，且其他验证条件也通过
-    if (password.value === "2025042801" && username.value === "2025042801") {
+    if (password.value === "dontletyouknow" && username.value === "dontletyouknow") {
         // console.log("pass1206");
         // 启用登录按钮
         loginButton.disabled = false;
