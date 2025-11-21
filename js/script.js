a1dea58c62a03ddc4fe8b7aa5490d98f5d6d38b3
wang-C109156201 <82928036+wang-C109156201@users.noreply.github.com>
2025-11-21 17:09:45 +0800

    Refactor login validation logic and enable button

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ loginButton.disabled = true;
 
 password.onkeyup = function () {
     // 如果密码为 "class1024"，且其他验证条件也通过
-    if (password.value === "20251117class" && username.value === "20251117class") {
+    if (password.value === "20251117class" && username.value === "1125") {
         // console.log("pass1206");
         // 启用登录按钮
         loginButton.disabled = false;
@@ -76,6 +76,7 @@ loginButton.onclick = function () {
     window.location.href = "./html/momhealth.html";
     alert("成功登入!")
 }
+
 
 
 
