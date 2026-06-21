a4eca62cb8d79b8abb38acee53b3694b04d40b57
wang-C109156201 <82928036+wang-C109156201@users.noreply.github.com>
2026-06-21 14:17:09 +0800

    Update password validation logic

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ loginButton.disabled = true;
 
 password.onkeyup = function () {
     // 如果密码为 "class1024"，且其他验证条件也通过
-    if (password.value === "20260511abc" && username.value === "20260511abc") {
+    if (password.value === "20260622" && username.value === "20260622") {
         // console.log("pass1206");
         // 启用登录按钮
         loginButton.disabled = false;
